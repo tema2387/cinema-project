f14fdb004b017f889c3c05d0331cec644c042bb1
tema2387 <artembaranov.00@mail.ru>
2022-08-03 16:58:25 +0300

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+    <header class="header">HEADER</header>
+</template>
+<script>
+export default {
+    name: 'Header',
+}
+</script>
+<style lang="less">
+.header {
+    text-align: center;
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    background: #ccc;
+    padding: 20px;
+    z-index: @mainZ;
+}
+</style>
---
+++ src/layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="site">
+        <Header />
+        <main class="site__content">
+            <Nuxt />
+        </main>
+        <Footer />
+    </div>
+</template>
+<script>
+export default {
+    name: 'Default',
+}
+</script>
